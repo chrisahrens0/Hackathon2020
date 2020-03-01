75e58400c50be2dcbf992df8af94381b5a921d98
Chris Ahrens <christopher00ahrens@gmail.com>
2020-02-29 17:48:50 -0800

    Update coord.h

--- backend/coord.h
+++ backend/coord.h
@@ -7,17 +7,20 @@
 struct Site {
     float x;
     float y;
+
+    Site(float xc, float yc){
+        x = xc;
+        y = yc;
+    }
+    Site(){
+        x = NULL;
+        y = NULL;
+    }
+
 };
 
-Site(float xc, float yc){
-    x = xc;
-    y = yc;
-}
-Site(){
-    x = NULL;
-    y = NULL;
-}
-
-float findDist(Site site1, Site site2){
+float findDist(Site* site1, Site* site2){
     return sqrt(((site1->x + site2->x)^2)*((site1->y + site2->y)^2));
 }
+
+#endif
